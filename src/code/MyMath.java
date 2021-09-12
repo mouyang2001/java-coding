26fe1e1ceaab01ce56b7b53a57850c7d883c3294
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-09-12 21:55:23 +1200

    create MyMath class

---
+++ src/code/MyMath.java
@@ -0,0 +1,11 @@
+package code;
+
+public class MyMath {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int minus(int a, int b) {
+        return a-b;
+    }
+}
