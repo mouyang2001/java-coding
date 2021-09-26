902dbfed3351e5ea7879a9884e80904504c6606c
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-09-27 00:58:35 +1300

    add tabulation solution code to fibonacci

--- src/code/Fibonacci.java
+++ src/code/Fibonacci.java
@@ -16,4 +16,15 @@ public class Fibonacci {
             return fibValue;
         }
     }
+
+    public int calculateT(int n) {
+        int[] table = new int[n+1];
+        table[1] = 1;
+        for (int i = 0; i <= n; i++) {
+            table[i+1] += table[i];
+            table[i+2] += table[i];
+        }
+
+        return table[n];
+    }
 }
