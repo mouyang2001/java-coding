ad24c0b37e6bc853a9bf9405be63eebe4c57708c
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-09-30 22:44:57 +1300

    add grid traveler problem code

--- src/code/GridTraveler.java
+++ src/code/GridTraveler.java
@@ -1,2 +1,29 @@
-package code;public class GridTraveler {
+package code;
+
+import java.util.HashMap;
+
+public class GridTraveler {
+    HashMap<String, Integer> memo;
+
+    public GridTraveler() {
+        memo = new HashMap<>();
+    }
+
+    /**
+     * Grid traveler problem recursive memoization.
+     * Uses a hash table to store and fetch key value data.
+     *
+     * @param r rows.
+     * @param c columns.
+     */
+    public int gridTraveler(int r, int c) {
+        String key = String.valueOf(r) + ',' + c;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        if (r == 1 && c == 1) return 1;
+        if (r == 0 || c == 0) return 0;
+
+        memo.put(key, gridTraveler(r-1, c) + gridTraveler(r, c-1));
+        return memo.get(key);
+    }
 }
