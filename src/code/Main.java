7bdcdaf29349cff38a2f116d793fc544f1f77a00
Matthew Ouyang <matthew.ouyang@beca.com>
2022-01-13 22:34:27 +1300

    Leetcode 3sum solution

--- src/code/Main.java
+++ src/code/Main.java
@@ -69,4 +69,37 @@ public class Main {
         return a;
     }
 
+    public static List<List<Integer>> threeSum(int [] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (nums[i] != nums[i-1])) {
+                // two sum sliding window technique.
+                int low = i + 1;
+                int high  = nums.length-1;
+                int sum = -nums[i];
+
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                        // Skip over duplicates.
+                        while (low < high && nums[low] == nums[low+1]) low++;
+                        while (low < high && nums[high] == nums[high-1]) high--;
+
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] > sum) {
+                        high--;
+                    } else  {
+                        low++;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
 }
