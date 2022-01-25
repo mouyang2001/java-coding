81fa5d67e980ab05e85e8db9a35727e1c9db6d47
Matthew Ouyang <matthew.ouyang@beca.com>
2022-01-25 23:30:19 +1300

    add removeElement problem

--- src/code/Main.java
+++ src/code/Main.java
@@ -8,5 +8,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(LeetCode.removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
     }
 }
