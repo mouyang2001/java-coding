2172436a70768deb2638424c5510fcb2e48f9570
Matthew Ouyang <matthew.ouyang@beca.com>
2022-02-14 20:53:44 +1300

    add prototype creational design pattern

--- src/code/patterns/design/creational/prototype/Circle.java
+++ src/code/patterns/design/creational/prototype/Circle.java
@@ -1,2 +1,27 @@
-package code.patterns.design.creational.prototype;public class Circle {
+package code.patterns.design.creational.prototype;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target); // Super calls parent constructor, Shape() {}
+        if (target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Circle) || !super.equals(object2)) return false;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
 }
